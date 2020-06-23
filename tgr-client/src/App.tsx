ebc36fbd7ef2e7ef2746039b3016212acdaf5e1e
richard.graham <richard.graham@xero.com>
2020-06-24 10:46:17 +1200

    added material-ui

--- tgr-client/src/App.tsx
+++ tgr-client/src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
+import Button from "@material-ui/core/Button";
 
 function App() {
-  return <div className="App">{/* <Counter /> */}</div>;
+  return <div className="App">{}</div>;
 }
 
 export default App;
